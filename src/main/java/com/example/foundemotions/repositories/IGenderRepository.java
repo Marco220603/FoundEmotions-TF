c9e23508a616d5f096a713055e12006698722591
Marco220603 <133729769+Marco220603@users.noreply.github.com>
2023-09-23 00:34:17 -0500

    reposriore

--- src/main/java/com/example/foundemotions/repositories/IGenderRepository.java
+++ src/main/java/com/example/foundemotions/repositories/IGenderRepository.java
@@ -2,7 +2,9 @@ package com.example.foundemotions.repositories;
 
 import com.example.foundemotions.entities.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IGenderRepository extends JpaRepository <Gender,Integer> {
 
 }
--- src/main/java/com/example/foundemotions/repositories/IPlaylistRepository.java
+++ src/main/java/com/example/foundemotions/repositories/IPlaylistRepository.java
@@ -2,7 +2,9 @@ package com.example.foundemotions.repositories;
 
 import com.example.foundemotions.entities.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IPlaylistRepository extends JpaRepository <Playlist,Integer> {
 
 
