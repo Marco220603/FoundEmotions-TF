c75f13c2df5ca451eba6280a23f8e6285842639a
JuniorAR <112164125+JuniorAR@users.noreply.github.com>
2023-09-22 23:57:06 -0500

    Artists
    Demo

--- src/main/java/com/example/foundemotions/entities/Artits.java
+++ src/main/java/com/example/foundemotions/entities/Artits.java
@@ -1,4 +1,57 @@
 package com.example.foundemotions.entities;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Artits")
 public class Artits {
+
+       @Id
+       @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+       private int id;
+
+       @Column(name = "nameArtits",length = 45,nullable = false)
+       private String nameArtits;
+
+
+       @ManyToOne
+       @JoinColumn(name = "genderid",referencedColumnName = "idgender")
+       private Gender genderid ;
+
+
+       public Artits(int id, String nameArtits, Gender genderid) {
+              this.id = id;
+              this.nameArtits = nameArtits;
+              this.genderid = genderid;
+       }
+
+       public Artits() {
+              
+       }
+
+       public int getId() {
+              return id;
+       }
+
+       public void setId(int id) {
+              this.id = id;
+       }
+
+       public String getNameArtits() {
+              return nameArtits;
+       }
+
+       public void setNameArtits(String nameArtits) {
+              this.nameArtits = nameArtits;
+       }
+
+       public Gender getGenderid() {
+              return genderid;
+       }
+
+       public void setGenderid(Gender genderid) {
+              this.genderid = genderid;
+       }
 }
--- src/main/java/com/example/foundemotions/entities/Gender.java
+++ src/main/java/com/example/foundemotions/entities/Gender.java
@@ -1,4 +1,41 @@
 package com.example.foundemotions.entities;
 
-public class Gender {
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="gender")
+public class Gender{
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private int id ;
+
+    @Column(name="nameGender",length = 40, nullable = false)
+    private String nameGender;
+
+    public Gender(int id, String nameGender) {
+        this.id = id;
+        this.nameGender = nameGender;
+    }
+
+    public Gender() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameGender() {
+        return nameGender;
+    }
+
+    public void setNameGender(String nameGender) {
+        this.nameGender = nameGender;
+    }
 }
