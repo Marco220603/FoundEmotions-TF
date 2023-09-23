f71fa2437983f503915280e4ac063a4012cf7920
Marco220603 <133729769+Marco220603@users.noreply.github.com>
2023-09-23 00:16:25 -0500

    finishh

--- src/main/java/com/example/foundemotions/entities/QuestionnaireResult.java
+++ src/main/java/com/example/foundemotions/entities/QuestionnaireResult.java
@@ -1,4 +1,85 @@
 package com.example.foundemotions.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "QuestionnaireResult")
 public class QuestionnaireResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToOne
+    @JoinColumn(name = "userid")
+    private User user;
+    @OneToMany
+    @JoinColumn(name = "questionsid")
+    private List<Questions> questions;
+    @Column(name = "rpta")
+    private int rpta;
+    @Column(name = "totalscore")
+    private int totalscore;
+    @OneToOne
+    @JoinColumn(name = "temperid")
+    private Temper temper;
+
+    public QuestionnaireResult() {
+    }
+
+    public QuestionnaireResult(int id, User user, List<Questions> questions, int rpta, int totalscore, Temper temper) {
+        this.id = id;
+        this.user = user;
+        this.questions = questions;
+        this.rpta = rpta;
+        this.totalscore = totalscore;
+        this.temper = temper;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Questions> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Questions> questions) {
+        this.questions = questions;
+    }
+
+    public int getRpta() {
+        return rpta;
+    }
+
+    public void setRpta(int rpta) {
+        this.rpta = rpta;
+    }
+
+    public int getTotalscore() {
+        return totalscore;
+    }
+
+    public void setTotalscore(int totalscore) {
+        this.totalscore = totalscore;
+    }
+
+    public Temper getTemper() {
+        return temper;
+    }
+
+    public void setTemper(Temper temper) {
+        this.temper = temper;
+    }
 }
--- src/main/java/com/example/foundemotions/entities/Questions.java
+++ src/main/java/com/example/foundemotions/entities/Questions.java
@@ -1,4 +1,81 @@
 package com.example.foundemotions.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Questions")
 public class Questions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "textquestion",nullable = false,length = 150)
+    private String textquestion;
+    @Column(name = "alternative1",nullable = false,length = 150)
+    private String alternative1;
+    @Column(name = "alternative2",nullable = false,length = 150)
+    private String alternative2;
+    @Column(name = "alternative3",nullable = false,length = 150)
+    private String alternative3;
+    @Column(name = "alternative4",nullable = false,length = 150)
+    private String alternative4;
+
+    public Questions() {
+    }
+
+    public Questions(int id, String textquestion, String alternative1, String alternative2, String alternative3, String alternative4) {
+        this.id = id;
+        this.textquestion = textquestion;
+        this.alternative1 = alternative1;
+        this.alternative2 = alternative2;
+        this.alternative3 = alternative3;
+        this.alternative4 = alternative4;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTextquestion() {
+        return textquestion;
+    }
+
+    public void setTextquestion(String textquestion) {
+        this.textquestion = textquestion;
+    }
+
+    public String getAlternative1() {
+        return alternative1;
+    }
+
+    public void setAlternative1(String alternative1) {
+        this.alternative1 = alternative1;
+    }
+
+    public String getAlternative2() {
+        return alternative2;
+    }
+
+    public void setAlternative2(String alternative2) {
+        this.alternative2 = alternative2;
+    }
+
+    public String getAlternative3() {
+        return alternative3;
+    }
+
+    public void setAlternative3(String alternative3) {
+        this.alternative3 = alternative3;
+    }
+
+    public String getAlternative4() {
+        return alternative4;
+    }
+
+    public void setAlternative4(String alternative4) {
+        this.alternative4 = alternative4;
+    }
 }
