217c94f7cd0a6775e31efba2f73748e94cc9a1f8
Marco220603 <133729769+Marco220603@users.noreply.github.com>
2023-09-23 01:18:53 -0500

    ds

--- src/main/java/com/example/foundemotions/controllers/TemperController.java
+++ src/main/java/com/example/foundemotions/controllers/TemperController.java
@@ -1,14 +1,11 @@
-package com.example.foundemotions.contrllers;
+package com.example.foundemotions.controllers;
 
 import com.example.foundemotions.dtos.TemperDTO;
 import com.example.foundemotions.entities.Temper;
 import com.example.foundemotions.serviceinterface.ITemperService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/temper")
@@ -22,4 +19,13 @@ public class TemperController {
         Temper t = m.map(dto,Temper.class);
         tS.insert(t);
     }
+    @PutMapping
+    private void modify(@RequestBody TemperDTO dto){
+        ModelMapper m = new ModelMapper();
+        Temper t = m.map(dto,Temper.class);
+        tS.insert(t);
+    }
+
+
+
 }
