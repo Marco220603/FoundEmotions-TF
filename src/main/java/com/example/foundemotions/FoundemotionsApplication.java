92d011fe999591126696d12e5e6a97692adcc960
Marco Andre <marcosponceboza@hotmail.com>
2023-09-22 22:24:27 -0500

    agregando archivo

---
+++ src/main/java/com/example/foundemotions/FoundemotionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.foundemotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundemotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundemotionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foundemotions/FoundemotionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foundemotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundemotionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
