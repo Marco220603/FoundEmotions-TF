6faba51e1dd6c7e02168bcf6ca8bccb4b60eba1f
JuniorAR <112164125+JuniorAR@users.noreply.github.com>
2023-09-23 00:45:09 -0500

    Service

--- src/main/java/com/example/foundemotions/serviceinterface/IArtistService.java
+++ src/main/java/com/example/foundemotions/serviceinterface/IArtistService.java
@@ -1,4 +1,14 @@
 package com.example.foundemotions.serviceinterface;
 
+import com.example.foundemotions.entities.Artist;
+
+import java.util.List;
+
 public interface IArtistService {
+
+    public void insert(Artist artist);
+    public List<Artist> list();
+    public void delete(int idArtist);
+    public Artist listarId(int idArtist);
+
 }
---
+++ src/main/java/com/example/foundemotions/serviceinterface/IPlaylistService.java
@@ -0,0 +1,13 @@
+package com.example.foundemotions.serviceinterface;
+
+import com.example.foundemotions.entities.Gender;
+import com.example.foundemotions.entities.Playlist;
+
+import java.util.List;
+
+public interface IPlaylistService {
+    public void insert(Playlist playlist);
+    public List<Playlist> list();
+    public void delete(int idPlaylist);
+    public Playlist listarId(int idPlaylist);
+}
