cde31f5272f1584b26b8fd6f8f9761fe87f06014
Marco220603 <133729769+Marco220603@users.noreply.github.com>
2023-09-23 00:33:40 -0500

    interface

--- src/main/java/com/example/foundemotions/repositories/IArtistRepository.java
+++ src/main/java/com/example/foundemotions/repositories/IArtistRepository.java
@@ -2,7 +2,9 @@ package com.example.foundemotions.repositories;
 
 import com.example.foundemotions.entities.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IArtistRepository extends JpaRepository<Artist,Integer> {
 
 
---
+++ src/main/java/com/example/foundemotions/serviceinterface/IArtistService.java
@@ -0,0 +1,4 @@
+package com.example.foundemotions.serviceinterface;
+
+public interface IArtistService {
+}
