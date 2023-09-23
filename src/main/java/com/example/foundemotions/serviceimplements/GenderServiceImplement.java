e4722fcf527a70773eec8304048df447a0b54061
JuniorAR <112164125+JuniorAR@users.noreply.github.com>
2023-09-23 01:09:09 -0500

    Gender

---
+++ src/main/java/com/example/foundemotions/serviceimplements/GenderServiceImplement.java
@@ -0,0 +1,32 @@
+package com.example.foundemotions.serviceimplements;
+
+import com.example.foundemotions.entities.Artist;
+import com.example.foundemotions.entities.Gender;
+import com.example.foundemotions.repositories.IGenderRepository;
+import com.example.foundemotions.serviceinterface.IGenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class GenderServiceImplement implements IGenderService {
+
+
+    @Autowired
+    private IGenderRepository igR ;
+
+
+    @Override
+    public void insert(Gender gender) { igR.save(gender); }
+
+    @Override
+    public List<Gender> list() {return igR.findAll();};
+
+    @Override
+    public void delete(int idGender) {igR.deleteById(idGender);};
+
+    @Override
+    public Gender listarId(int idGender){return igR.findById(idGender).orElse(new Gender()) ;}
+}
