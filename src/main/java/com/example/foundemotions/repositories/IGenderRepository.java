eb60e6386001d310018532cc63b3b45e12e8e079
papitaconhuevo <143817798+papitaconhuevo@users.noreply.github.com>
2023-09-24 22:01:34 -0500

    Update IGenderRepository.java

--- src/main/java/com/example/foundemotions/repositories/IGenderRepository.java
+++ src/main/java/com/example/foundemotions/repositories/IGenderRepository.java
@@ -7,4 +7,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IGenderRepository extends JpaRepository <Gender,Integer> {
 
+  @Query("SELECT gender.nameGender AS genero, COUNT(artist.id) AS cantidad_de_artistas " +
+            "FROM Gender gender " +
+            "JOIN Artist artist ON gender.id = artist.gender.id " +
+            "GROUP BY gender.nameGender " +
+            "ORDER BY cantidad_de_artistas DESC")
+    List<String[]> findGenderWithMostArtists();
+
 }
